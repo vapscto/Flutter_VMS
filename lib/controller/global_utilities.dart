da72395f358dc12238830fa349e93ef9fba805f3
tapanpadhi25 <tapanpadhi25@gmail.com>
2023-10-26 15:59:01 +0530

    changes

--- lib/controller/global_utilities.dart
+++ lib/controller/global_utilities.dart
@@ -519,12 +519,12 @@ void openMappedPages(
     BuildContext context,
     HwCwNbController hwCwNbController) {
   logger.i(pageName);
-  // if (pageName == "Appointment Approval") {
-  //   Get.to(() => SalesReportHomeScreen(
-  //       loginSuccessModel: loginSuccessModel,
-  //       mskoolController: mskoolController));
-  //   return;
-  // }
+  if (pageName == "Appointment Approval") {
+    Get.to(() => SalesReportHomeScreen(
+        loginSuccessModel: loginSuccessModel,
+        mskoolController: mskoolController));
+    return;
+  }
   if (pageName == "Attendance") {
     Get.to(() => AttendanceHomeScreen(
         loginSuccessModel: loginSuccessModel,
