b3814a1b8e185256a5eb04cbaf4d362e4627d181
tapanpadhi25 <tapanpadhi25@gmail.com>
2023-11-13 17:46:41 +0530

    changes

--- lib/vms/issue_manager/planner_creation/widgets/planner_create.dart
+++ lib/vms/issue_manager/planner_creation/widgets/planner_create.dart
@@ -802,10 +802,10 @@ class _PlannerCreateWidgetState extends State<PlannerCreateWidget> {
                               } else if (plannedEffort <
                                   double.parse((totalday * 8).toString())) {
                                 Get.dialog(showPopup());
-                              } else if (plannerCreationController
-                                      .isPlannerCreate.value ==
-                                  false) {
-                                Get.dialog(plannerNotCreate());
+                                // } else if (plannerCreationController
+                                //         .isPlannerCreate.value ==
+                                //     false) {
+                                //   Get.dialog(plannerNotCreate());
                               } else {
                                 savePlanner();
                               }
