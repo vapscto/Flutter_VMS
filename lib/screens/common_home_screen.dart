a297acf1e0927d99423be8a61d008a6b6c6e7dff
tapanpadhi25 <tapanpadhi25@gmail.com>
2023-11-27 16:51:00 +0530

    changes

--- lib/screens/common_home_screen.dart
+++ lib/screens/common_home_screen.dart
@@ -129,101 +129,114 @@ class _CommonHomeScreenState extends State<CommonHomeScreen> {
     super.dispose();
   }
 
+  Future<bool> _onWillPop() async {
+    if (pageController.page! > 0) {
+      pageController.animateTo(pageController.page! - 1,
+          duration: const Duration(microseconds: 100),
+          curve: Curves.fastLinearToSlowEaseIn);
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffold,
-      appBar: (selectedPage.value == 0)
-          ? AppBar(
-              centerTitle: true,
-              elevation: 0,
-              leading: IconButton(
-                icon: SvgPicture.asset('assets/svg/menu.svg'),
-                onPressed: () {
-                  _scaffold.currentState!.openDrawer();
-                },
-              ),
-              title: Obx(() {
-                return Text(homePage.elementAt(selectedPage.value).title);
-              }),
-              actions: [
-                IconButton(
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        key: _scaffold,
+        appBar: (selectedPage.value == 0)
+            ? AppBar(
+                centerTitle: true,
+                elevation: 0,
+                leading: IconButton(
+                  icon: SvgPicture.asset('assets/svg/menu.svg'),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) {
-                          return NotificationScreen(
-                            // appBarTitle: "Notice",
-                            loginSuccessModel: widget.loginSuccessModel,
-                            mskoolController: widget.mskoolController,
-                            openFor: widget.loginSuccessModel.roleforlogin!
-                                .toLowerCase(),
-                            hwCwNbController: hwCwNbController,
-                          );
-                          // hwCwNbController: hwCwNbController);
-                        },
-                      ),
-                    );
+                    _scaffold.currentState!.openDrawer();
                   },
-                  icon: SvgPicture.asset(
-                    'assets/svg/bell.svg',
+                ),
+                title: Obx(() {
+                  return Text(homePage.elementAt(selectedPage.value).title);
+                }),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) {
+                            return NotificationScreen(
+                              // appBarTitle: "Notice",
+                              loginSuccessModel: widget.loginSuccessModel,
+                              mskoolController: widget.mskoolController,
+                              openFor: widget.loginSuccessModel.roleforlogin!
+                                  .toLowerCase(),
+                              hwCwNbController: hwCwNbController,
+                            );
+                            // hwCwNbController: hwCwNbController);
+                          },
+                        ),
+                      );
+                    },
+                    icon: SvgPicture.asset(
+                      'assets/svg/bell.svg',
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 6.0,
-                ),
-              ],
-            )
-          : null,
-      drawer: Drawer(
-        child: CommonDrawer(
-          loginSuccessModel: widget.loginSuccessModel,
-          hwCwNbController: hwCwNbController,
-          mskoolController: widget.mskoolController,
+                  const SizedBox(
+                    width: 6.0,
+                  ),
+                ],
+              )
+            : null,
+        drawer: Drawer(
+          child: CommonDrawer(
+            loginSuccessModel: widget.loginSuccessModel,
+            hwCwNbController: hwCwNbController,
+            mskoolController: widget.mskoolController,
+          ),
         ),
-      ),
-      body: PageView.builder(
-        controller: pageController,
-        itemCount: homePage.length,
-        itemBuilder: (_, index) {
-          return homePage.elementAt(index).page;
-        },
-        onPageChanged: (v) {
-          setState(() {
-            selectedPage.value = v;
-          });
-        },
-      ),
-      bottomNavigationBar: Material(
-        elevation: 10.0,
-        color: Theme.of(context).scaffoldBackgroundColor,
-        child: Obx(() {
-          return SalomonBottomBar(
-            currentIndex: selectedPage.value,
-            items: [
-              ...List.generate(
-                  homePage.length,
-                  (index) => SalomonBottomBarItem(
-                      unselectedColor: const Color(0xFFC0C0C0),
-                      selectedColor: homePage.elementAt(index).selectedColor,
-                      icon: Image.asset(
-                        homePage.elementAt(index).icon,
-                        height: homePage.elementAt(index).size ?? 30,
-                        color: selectedPage.value == index
-                            ? homePage.elementAt(index).selectedColor
-                            : const Color(0xFFC0C0C0),
-                      ),
-                      title: Text(homePage.elementAt(index).title)))
-            ],
-            onTap: (v) {
+        body: PageView.builder(
+          controller: pageController,
+          itemCount: homePage.length,
+          itemBuilder: (_, index) {
+            return homePage.elementAt(index).page;
+          },
+          onPageChanged: (v) {
+            setState(() {
               selectedPage.value = v;
-              pageController.animateToPage(v,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.fastLinearToSlowEaseIn);
-            },
-          );
-        }),
+            });
+          },
+        ),
+        bottomNavigationBar: Material(
+          elevation: 10.0,
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: Obx(() {
+            return SalomonBottomBar(
+              currentIndex: selectedPage.value,
+              items: [
+                ...List.generate(
+                    homePage.length,
+                    (index) => SalomonBottomBarItem(
+                        unselectedColor: const Color(0xFFC0C0C0),
+                        selectedColor: homePage.elementAt(index).selectedColor,
+                        icon: Image.asset(
+                          homePage.elementAt(index).icon,
+                          height: homePage.elementAt(index).size ?? 30,
+                          color: selectedPage.value == index
+                              ? homePage.elementAt(index).selectedColor
+                              : const Color(0xFFC0C0C0),
+                        ),
+                        title: Text(homePage.elementAt(index).title)))
+              ],
+              onTap: (v) {
+                selectedPage.value = v;
+                pageController.animateToPage(v,
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.fastLinearToSlowEaseIn);
+              },
+            );
+          }),
+        ),
       ),
     );
   }
