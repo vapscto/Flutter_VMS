d00daa32ba2d3daedba8503131b58f091a97bc9f
tapanpadhi25 <tapanpadhi25@gmail.com>
2023-12-12 19:00:08 +0530

    privileage leave issue fixes

--- lib/vms/online_leave/api/ol_featch_api.dart
+++ lib/vms/online_leave/api/ol_featch_api.dart
@@ -4,30 +4,27 @@ import 'package:m_skool_flutter/controller/global_utilities.dart';
 import 'package:m_skool_flutter/main.dart';
 import 'package:m_skool_flutter/vms/online_leave/controller/ol_controller.dart';
 import 'package:m_skool_flutter/vms/online_leave/model/optional_leave_model.dart';
- 
-Future<void> getOptionalLeave({
-    required int miId,
+
+Future<void> getOptionalLeave(
+    {required int miId,
     required int userId,
     required int asmayId,
     required String base,
-    required OpetionLeaveController controller
-  }) async {
-    final Dio ins = getGlobalDio();
-    final String apiUrl =  base + URLS.appliedLeaves;
-    logger.e(apiUrl);
-    try {
-      final Response response = await ins
-          .post(apiUrl, options: Options(headers: getSession()), data: {
-        "MI_Id": miId,
-        "UserId": userId,
-        "asmay_id": asmayId
-   });
-     logger.d(apiUrl);
-     OpetionLeaveModel olList = OpetionLeaveModel.fromJson(response.data['optionalleavedata']);
-     controller.optionalLeaveList.addAll(olList.values!);
-     } on DioError catch (e) {
-     logger.e(e.message); 
-    } on Exception catch (e) {
-     logger.e(e);
-    }
+    required OpetionLeaveController controller}) async {
+  final Dio ins = getGlobalDio();
+  final String apiUrl = base + URLS.appliedLeaves;
+  logger.e(apiUrl);
+  try {
+    final Response response = await ins.post(apiUrl,
+        options: Options(headers: getSession()),
+        data: {"MI_Id": miId, "UserId": userId, "asmay_id": asmayId});
+    logger.d(apiUrl);
+    OpetionLeaveModel olList =
+        OpetionLeaveModel.fromJson(response.data['optionalleavedata']);
+    controller.getOptionalLeave(olList.values!);
+  } on DioError catch (e) {
+    logger.e(e.message);
+  } on Exception catch (e) {
+    logger.e(e);
   }
+}
