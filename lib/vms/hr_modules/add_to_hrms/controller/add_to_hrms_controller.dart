0156abfdaae86a513db10e7ea0b3212dc23e8372
tapanpadhi25 <tapanpadhi25@gmail.com>
2024-04-11 17:42:05 +0530

    add to hrms

--- lib/vms/hr_modules/add_to_hrms/controller/add_to_hrms_controller.dart
+++ lib/vms/hr_modules/add_to_hrms/controller/add_to_hrms_controller.dart
@@ -1,6 +1,14 @@
 import 'package:get/get.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/add_to_hrmd_deg.dart';
 import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/add_to_hrms_candidate_model.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/add_to_hrms_dep_model.dart';
 import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/add_to_hrms_emp_type.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/cast_list_model.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/company_list_model.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/grade_list_model.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/group_type_list.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/marital_list_model.dart';
+import 'package:m_skool_flutter/vms/hr_modules/add_to_hrms/model/religion_model.dart';
 
 class AddToHRMSConttroller extends GetxController {
   RxBool isLoading = RxBool(false);
@@ -17,4 +25,15 @@ class AddToHRMSConttroller extends GetxController {
       <AddToHrmsCandidateListModelValues>[].obs;
   RxList<AddToHrmsEmpTypeModelValues> empTypeList =
       <AddToHrmsEmpTypeModelValues>[].obs;
+  RxList<ReligionListModelValues> religionlist =
+      <ReligionListModelValues>[].obs;
+  RxList<CastListModelValues> castList = <CastListModelValues>[].obs;
+  RxList<MaritalListModelValues> maritalList = <MaritalListModelValues>[].obs;
+  RxList<AddToHrmsDepListModelValues> depList =
+      <AddToHrmsDepListModelValues>[].obs;
+  RxList<AddToHrmsDegModelValues> degList = <AddToHrmsDegModelValues>[].obs;
+  RxList<GradeListModelValues> gradeList = <GradeListModelValues>[].obs;
+  RxList<GroupTypeListModelValues> groupTypeList =
+      <GroupTypeListModelValues>[].obs;
+  RxList<CompanyListModelValues> companyList = <CompanyListModelValues>[].obs;
 }
